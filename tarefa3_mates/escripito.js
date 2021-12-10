462c354cd2f19c527a346ba125fea05aa9966390
nahomigonzalez <92986087+nahomigonzalez@users.noreply.github.com>
2021-12-10 13:54:28 +0100

    Add files via upload

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,23 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+
+var moito = 9;
+    var raiz = Math.sqrt (moito);
+    alert(raiz);
+
+var pouco = 5;
+    var cuadrado = Math.pow (pouco,2);
+    alert(cuadrado);
+
+alert(Math.PI);
+
+var outro = 7;
+    var todo = Math.min (moito, pouco, outro);
+    alert(todo);
+
+var aleatorio = Math.random (0, 3);
+alert(aleatorio);
